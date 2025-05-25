0a6958b43c5483b1acff270e2b50810114385511
Okiso-j <art.ignatencko34@gmail.com>
2025-05-25 18:17:08 +0300

    fix

--- src/components/TimeBox/TimeBox.tsx
+++ src/components/TimeBox/TimeBox.tsx
@@ -1,8 +1,15 @@
+import { Marck_Script } from 'next/font/google';
+
+const marck = Marck_Script({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 export default function TimeBox({ label, value }: { label: string; value: number }) {
   return (
     <div className="flex flex-col items-center text-pink-800">
-      <span className="text-[24px] font-bold">{value}</span>
-      <span className="text-[12px]">{label}</span>
+      <span className={`text-[45px] leading-[1] font-bold ${marck.className}`}>{value}</span>
+      <span className={`text-[25px] leading-[1] ${marck.className}`}>{label}</span>
     </div>
   );
 }
